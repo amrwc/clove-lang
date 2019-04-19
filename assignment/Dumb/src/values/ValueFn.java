548e2f5d99a70c5c1c0ec0bd9403fadedd4bedff
amrwc <42574496+amrwc@users.noreply.github.com>
2019-04-19 01:23:50 +0100

    ValueFn
    * Attempt to fix ValueFn
    - add alternative implementation of FnVal,
    - start improving ValueFn class -- it has to include FunctionDefinition and resolve to a Value to work properly inside of anonymous objects and while being passed as a parameter.
    
    * Advance with the fix -- the parameters are not accounted for correctly
    
    * Fix ValueFn
    - allow it to work with anonymous object (NOTE: It requires special treatment inside the grammar.),
    - set it up to evaluate to a ValueFn and start solving the argument passing problem.
    
    * Solve the function as argument problem
    - fix an issue where the argument being a ValueFn caused issues with being null.
    
    The ValueFn passed down as an argument is now evaluated as a normal ValueFn and assigned a UUID. When the parameter it applies to is being invoked, the invokation realises that the function with the parameter's name does not exist, and therefore it starts a lookup. In the lookup, it traverses the tree to access the relevant parameters, the uses the parameter's index to access the UUID-named function definition and swaps the failed function definition with the one found in the map.
    
    This solution uses two nasty hacks:
    - global, Parser-wide variable with the function aliases map -- it's there to be accessible throughout the run of the program, and
    - global, Parser-wide variable with the last Identifier's token vale -- it's a hacky way to get the parameter's name, since the ASTIdentifier method does not return anything by default.

--- assignment/Dumb/src/values/ValueFn.java
+++ assignment/Dumb/src/values/ValueFn.java
@@ -1,131 +1,28 @@
 package values;
 
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Vector;
+public class ValueFn extends ValueAbstract {
+	private String fnname;
+	private int scope;
 
-import interpreter.ExceptionSemantic;
-import parser.ast.SimpleNode;
+	public ValueFn() {}
 
-public class ValueFn extends ValueAbstract implements Comparable<Object>, Serializable {
-	private static final long serialVersionUID = 0;
-
-	private String parmSignature = "";
-	private Vector<String> parameters = new Vector<String>();
-	private HashMap<String, Integer> slots = new HashMap<String, Integer>();
-	private HashMap<String, ValueFn> functions = new HashMap<String, ValueFn>();
-	private SimpleNode ASTFunctionBody = null;
-	private SimpleNode ASTFunctionReturnExpression = null;
-	private int depth;
-
-	/** Ctor for function definition. */
-	public ValueFn(int level) {
-		depth = level;
+	public ValueFn(String fnname, int scope) {
+		this.fnname = fnname;
+		this.scope = scope;
 	}
 
-	/** Get the depth of this definition.
-	 * 0 - root or main scope
-	 * 1 - definition inside root or main scope
-	 * 2 - definition inside 1
-	 * n - etc.
-	 */
-	public int getLevel() {
-		return depth;
-	}
-
-	/** Required by ValueAbstract. */
+	@Override
 	public String getName() {
-		return null;
+		return fnname;
+	}
+	
+	public int getScope() {
+		return scope;
 	}
 
-	/** Required by ValueAbstract. */
+	@Override
 	public int compare(Value v) {
+		// TODO Auto-generated method stub
 		return 0;
-	}
-
-	/** Set the function body of this function. */
-	void setFunctionBody(SimpleNode node) {
-		ASTFunctionBody = node;
-	}
-	
-	/** Get the function body of this function. */
-	public SimpleNode getFunctionBody() {
-		return ASTFunctionBody;
-	}
-	
-	/** Set the return expression of this function. */
-	void setFunctionReturnExpression(SimpleNode node) {
-		ASTFunctionReturnExpression = node;
-	}
-	
-	/** Get the return expression of this function. */
-	public SimpleNode getFunctionReturnExpression() {
-		return ASTFunctionReturnExpression;
-	}
-	
-	/** Get the signature of this function. */
-	public String getSignature() {
-		return (hasReturn() ? "value " : "") + getName() + "(" + parmSignature + ")";
-	}
-	
-	/** True if this function has a return value. */
-	public boolean hasReturn() {
-		return (ASTFunctionReturnExpression != null);
-	}
-	
-	public int compareTo(Object o) {
-		return 0;
-	}
-	
-	/** Get count of parameters. */
-	public int getParameterCount() {
-		return parameters.size();
-	}
-	
-	/** Get the name of the ith parameter. */
-	String getParameterName(int i) {
-		return parameters.get(i);
-	}
-	
-	/** Define a parameter. */
-	void defineParameter(String name) {
-		if (parameters.contains(name))
-			throw new ExceptionSemantic("Parameter " + name + " already exists in function " + getName());
-		parameters.add(name);
-		parmSignature += ((parmSignature.length()==0) ? name : (", " + name));
-		defineVariable(name);
-	}
-	
-	/** Get count of local variables and parameters. */
-	public int getLocalCount() {
-		return slots.size();
-	}
-	
-	/** Get the storage slot number of a given variable or parm.  Return -1 if it doesn't exist. */
-	public int getLocalSlotNumber(String name) {
-		Integer slot = slots.get(name);
-		if (slot == null)
-			return -1;
-		return slot.intValue();
-	}
-	
-	/** Define a variable.  Return its slot number. */
-	public int defineVariable(String name) {
-		Integer slot = slots.get(name);
-		if (slot != null)
-			return slot.intValue();
-		int slotNumber = slots.size();
-		slots.put(name, Integer.valueOf(slotNumber));
-		return slotNumber;
-	}	
-	
-	/** Add an inner function definition. */
-	public void addFunction(ValueFn definition) {
-		functions.put(definition.getName(), definition);
-	}
-	
-	/** Find an inner function definition.  Return null if it doesn't exist. */
-	ValueFn findFunction(String name) {
-		return functions.get(name);
 	}
 }
