775a7a7831a25de457216ec0383ac095fd515a17
Adam Morawiec <42574496+amrwc@users.noreply.github.com>
2019-04-25 15:32:47 +0100

    Start implementing List data structure
    - add grammar for declaring and dereferencing a ValueList,
    - implement ValueList declaration/initialisation in the Parser
    - add a test for ValueList declaration.

---
+++ assignment/Dumb/src/values/ValueList.java
@@ -0,0 +1,42 @@
+package values;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class ValueList extends ValueAbstract {
+
+	private ArrayList<Value> internalValue = new ArrayList<Value>();
+	
+	public ValueList() {}
+	
+	public String getName() {
+		return "List";
+	}
+
+	public int compare(Value v) {
+		// TODO Auto-generated method stub
+//		return internalValue.compareTo(v);
+		return 0;
+	}
+
+	public void append(Value v) {
+//		System.out.print("DEBUG, ValueList.append: "); // DEBUG:
+//		System.out.println(internalValue.get(i)); // DEBUG:
+		internalValue.add(v);
+	}
+	
+	public Value get(int i) {
+//		System.out.print("DEBUG, ValueList.get: "); // DEBUG:
+//		System.out.println(internalValue.get(i)); // DEBUG:
+//		if (internalValue.)
+		return internalValue.get(i);
+	}
+
+	public String toString() {
+		// https://stackoverflow.com/a/23183963/10620237
+		return internalValue
+				.stream()
+				.map(Object::toString)
+				.collect(Collectors.joining(", "));
+	}
+}
