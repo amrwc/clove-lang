d00865c0e1b57ae50cd9982c6dcfa14375e68b19
Adam Morawiec <42574496+amrwc@users.noreply.github.com>
2019-04-26 18:46:37 +0100

    Implement compare() method in the ValueObject class

--- assignment/Dumb/src/values/ValueObject.java
+++ assignment/Dumb/src/values/ValueObject.java
@@ -2,38 +2,34 @@ package values;
 
 import java.util.HashMap;
 
+/**
+ * @see https://docs.oracle.com/javase/8/docs/api/java/util/HashMap.html
+ * @author amrwc
+ */
 public class ValueObject extends ValueAbstract {
 	
 	private HashMap<String, Value> internalValue = new HashMap<String, Value>();
-	
+
 	public ValueObject() {}
-	
+
 	public String getName() {
 		return "Object";
 	}
 
 	public int compare(Value v) {
-		// TODO Auto-generated method stub
-//		return internalValue.compareTo(v);
-		return 0;
+		HashMap<String, Value> map = ((ValueObject) v).internalValue;
+		return internalValue.equals(map) ? 0 : 1;
 	}
-	
+
 	public void add(String name, Value v) {
-//		System.out.print("DEBUG, ValueObject.add: "); // DEBUG:
-//		System.out.println(internalValue.get(name)); // DEBUG:
-		internalValue.put(name, v);
+		internalValue.putIfAbsent(name, v);
 	}
-	
+
 	public Value get(String name) {
-//		System.out.print("DEBUG, ValueObject.get: "); // DEBUG:
-//		System.out.println(internalValue.get(name)); // DEBUG:
-//		if (internalValue.)
 		return internalValue.get(name);
 	}
-	
+
 	public void set(String name, Value v) {
-//		System.out.print("DEBUG, ValueObject.set: "); // DEBUG:
-//		System.out.println("oldValue: " + internalValue.put(name, v)); // DEBUG:
 		internalValue.put(name, v);
 	}
 
