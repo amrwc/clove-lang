657d442f5b2486d50f5499bc4eae59e2feb58167
amrwc <42574496+amrwc@users.noreply.github.com>
2019-05-15 17:20:53 +0200

    Refactor the main files to match each-other's function ordering

--- assignment/Clove/src/interpreter/ParserDebugger.java
+++ assignment/Clove/src/interpreter/ParserDebugger.java
@@ -3,9 +3,8 @@ package interpreter;
 import parser.ast.*;
 
 public class ParserDebugger implements CloveVisitor {
-	
 	private int indent = 0;
-	
+
 	private String indentString() {
 		StringBuffer sb = new StringBuffer();
 		for (int i = 0; i < indent; ++i) {
@@ -13,7 +12,7 @@ public class ParserDebugger implements CloveVisitor {
 		}
 		return sb.toString();
 	}
-	
+
 	/** Debugging dump of a node. */
 	private Object dump(SimpleNode node, Object data) {
 		System.out.println(indentString() + node);
@@ -22,20 +21,50 @@ public class ParserDebugger implements CloveVisitor {
 		--indent;
 		return data;		
 	}
-	
+
 	public Object visit(SimpleNode node, Object data) {
 		System.out.println(node + ": acceptor not implemented in subclass?");
 		return data;
 	}
-	
+
 	// Execute a Clove program
 	public Object visit(ASTCode node, Object data) {
 		dump(node, data);
 		return data;
 	}
-	
+
+
+
+	/***********************************************
+	 *                 Statements                  *
+	 ***********************************************/
+
 	// Execute a statement
 	public Object visit(ASTStatement node, Object data) {
+		dump(node, data);
+		return data;
+	}
+
+	// Function call
+	public Object visit(ASTCall node, Object data) {
+		dump(node, data);
+		return data;
+	}
+
+	public Object visit(ASTDefinition node, Object data) {
+		dump(node, data);
+		return null;
+	}
+
+	// Execute an assignment statement, by popping a value off the stack and assigning it
+	// to a variable.
+	public Object visit(ASTAssignment node, Object data) {
+		dump(node, data);
+		return data;
+	}
+
+	// Function definition
+	public Object visit(ASTFunctionDefinition node, Object data) {
 		dump(node, data);
 		return data;
 	}
@@ -51,80 +80,101 @@ public class ParserDebugger implements CloveVisitor {
 		dump(node, data);
 		return data;
 	}
-	
-	// Function definition parameter list
-	public Object visit(ASTParmlist node, Object data) {
-		dump(node, data);
-		return data;
-	}
-	
-	// Function body
-	public Object visit(ASTFnBody node, Object data) {
-		dump(node, data);
-		return data;
-	}
-	
-	// Function definition
-	public Object visit(ASTFnDef node, Object data) {
-		dump(node, data);
-		return data;
-	}
-	
-	// Function return expression
-	public Object visit(ASTReturnExpression node, Object data) {
-		dump(node, data);
-		return data;
-	}
-	
-	// Function argument list
-	public Object visit(ASTArgList node, Object data) {
-		dump(node, data);
-		return data;
-	}
-	
-	// Function call
-	public Object visit(ASTCall node, Object data) {
-		dump(node, data);
-		return data;
-	}
-	
-	// Function invocation in an expression
-	public Object visit(ASTFnInvoke node, Object data) {
-		dump(node, data);
-		return data;
-	}
-	
-	// Dereference a variable, and push its value onto the stack
-	public Object visit(ASTDereference node, Object data) {
-		dump(node, data);
-		return data;
-	}
-	
+
 	// Execute a FOR loop
 	public Object visit(ASTForLoop node, Object data) {
 		dump(node, data);
 		return data;
 	}
-	
+
+	public Object visit(ASTWhileLoop node, Object data) {
+		dump(node, data);
+		return data;
+	}
+
+	// Execute the WRITE statement
+	public Object visit(ASTWrite node, Object data) {
+		dump(node, data);
+		return data;
+	}
+
+	public Object visit(ASTQuit node, Object data) {
+		dump(node, data);
+		return data;
+	}
+
+	public Object visit(ASTProtoInvoke node, Object data) {
+		dump(node, data);
+		return data;
+	}
+
+	public Object visit(ASTIncrementDecrement node, Object data) {
+		dump(node, data);
+		return null;
+	}
+
+	public Object visit(ASTDeclaration node, Object data) {
+		dump(node, data);
+		return null;
+	}
+
+	public Object visit(ASTHttp node, Object data) {
+		dump(node, data);
+		return null;
+	}
+
+	public Object visit(ASTFile node, Object data) {
+		dump(node, data);
+		return null;
+	}
+
+
+
+	/***********************************************
+	 *               Sub-statements                *
+	 ***********************************************/
+
 	// Process an identifier
 	// This doesn't do anything, but needs to be here because we need an ASTIdentifier node.
 	public Object visit(ASTIdentifier node, Object data) {
 		dump(node, data);
 		return data;
 	}
-	
-	// Execute the WRITE statement
-	public Object visit(ASTWrite node, Object data) {
+
+	public Object visit(ASTConstInit node, Object data) {
+		dump(node, data);
+		return null;
+	}
+
+	// Function argument list
+	public Object visit(ASTArgumentList node, Object data) {
 		dump(node, data);
 		return data;
 	}
-	
-	// Execute an assignment statement, by popping a value off the stack and assigning it
-	// to a variable.
-	public Object visit(ASTAssignment node, Object data) {
+
+	// Function definition parameter list
+	public Object visit(ASTParameterList node, Object data) {
 		dump(node, data);
 		return data;
 	}
+
+	// Function body
+	public Object visit(ASTFunctionBody node, Object data) {
+		dump(node, data);
+		return data;
+	}
+
+	// Function return expression
+	public Object visit(ASTReturnExpression node, Object data) {
+		dump(node, data);
+		return data;
+	}
+
+
+
+	/***********************************************
+	 *                Expressions                  *
+	 ***********************************************/
 
 	// OR
 	public Object visit(ASTOr node, Object data) {
@@ -222,47 +272,60 @@ public class ParserDebugger implements CloveVisitor {
 		return data;
 	}
 
-	// Push string literal to stack
-	public Object visit(ASTCharacter node, Object data) {
+	// Function invocation in an expression
+	public Object visit(ASTFunctionInvocation node, Object data) {
 		dump(node, data);
 		return data;
 	}
-	
-	// Push integer literal to stack
+
+	// Dereference a variable, and push its value onto the stack
+	public Object visit(ASTDereference node, Object data) {
+		dump(node, data);
+		return data;
+	}
+
+	public Object visit(ASTGetArgs node, Object data) {
+		dump(node, data);
+		return null;
+	}
+
+	public Object visit(ASTRandom node, Object data) {
+		dump(node, data);
+		return null;
+	}
+
+
+
+	/***********************************************
+	 *                 Literals                    *
+	 ***********************************************/
+
 	public Object visit(ASTInteger node, Object data) {
 		dump(node, data);
 		return data;
 	}
 
-	// Push floating point literal to stack
+	public Object visit(ASTCharacter node, Object data) {
+		dump(node, data);
+		return data;
+	}
+
 	public Object visit(ASTRational node, Object data) {
 		dump(node, data);
 		return data;
 	}
 
-	// Push true literal to stack
 	public Object visit(ASTTrue node, Object data) {
 		dump(node, data);
 		return data;
 	}
 
-	// Push false literal to stack
 	public Object visit(ASTFalse node, Object data) {
 		dump(node, data);
 		return data;
 	}
 
-	public Object visit(ASTWhileLoop node, Object data) {
-		dump(node, data);
-		return data;
-	}
-
-	public Object visit(ASTQuit node, Object data) {
-		dump(node, data);
-		return data;
-	}
-
-	public Object visit(ASTFnVal node, Object data) {
+	public Object visit(ASTValueFunction node, Object data) {
 		dump(node, data);
 		return data;
 	}
@@ -275,50 +338,5 @@ public class ParserDebugger implements CloveVisitor {
 	public Object visit(ASTValueList node, Object data) {
 		dump(node, data);
 		return data;
-	}
-
-	public Object visit(ASTProtoInvoke node, Object data) {
-		dump(node, data);
-		return data;
-	}
-
-	public Object visit(ASTDefinition node, Object data) {
-		dump(node, data);
-		return null;
-	}
-
-	public Object visit(ASTIncrementDecrement node, Object data) {
-		dump(node, data);
-		return null;
-	}
-
-	public Object visit(ASTGetArgs node, Object data) {
-		dump(node, data);
-		return null;
-	}
-
-	public Object visit(ASTConstInit node, Object data) {
-		dump(node, data);
-		return null;
-	}
-
-	public Object visit(ASTDeclaration node, Object data) {
-		dump(node, data);
-		return null;
-	}
-
-	public Object visit(ASTHttp node, Object data) {
-		dump(node, data);
-		return null;
-	}
-
-	public Object visit(ASTFile node, Object data) {
-		dump(node, data);
-		return null;
-	}
-
-	public Object visit(ASTRandom node, Object data) {
-		dump(node, data);
-		return null;
 	}
 }
