5053f116127a0f8c55b2555a50cc73ef32bc4316
amrwc <42574496+amrwc@users.noreply.github.com>
2019-05-24 16:34:33 +0200

    Get simple Reflections to work
    - add tests,
    - add example6,
    - clean up the ValueReflection class.

--- assignment/Clove/src/values/ValueReflection.java
+++ assignment/Clove/src/values/ValueReflection.java
@@ -2,10 +2,8 @@ package values;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.HashMap;
 
-import interpreter.ExceptionSemantic;
 import interpreter.Parser;
 import parser.ast.SimpleNode;
 
@@ -14,6 +12,15 @@ import parser.ast.SimpleNode;
  * @read https://www.geeksforgeeks.org/reflection-in-java/
  * @read http://tutorials.jenkov.com/java-reflection/index.html
  * @author amrwc
+ * 
+ * TODO:
+ * - Refactor the parseArgs() methods and perhaps merge them,
+ *   or rename appropriately.
+ * - Implement execProto() method, or remove it if it is
+ *   for no use.
+ * - Add a constructor to Value that will take Object and do
+ *   all the casting that is done in getCorrespondingValue
+ *   -- just move this method into the Value interface.
  */
 public class ValueReflection extends ValueAbstract {
 	private Class<?> theClass;
@@ -39,14 +46,6 @@ public class ValueReflection extends ValueAbstract {
 			// Create an instance of the class using the arguments
 			// and their matching constructor.
 			instance = constructor.newInstance(args.get("args"));
-
-//			URL url = new URL("https://jsonplaceholder.typicode.com/posts/1");
-//			Object conn = Class.forName("java.net.HttpURLConnection");
-//			conn = ((Class<?>) conn).cast(url.openConnection());
-//System.out.println("INSANITY: " + conn.getClass());
-// TODO: Change Class<?> for theClass to Object and then cast it whenever necessary.
-//       This way, it's possible to cast shit like the above.
-//       OR: only do this when casting is necessary (make a 'cast' token?).
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -61,8 +60,118 @@ public class ValueReflection extends ValueAbstract {
 	 */
 	public ValueReflection(Object newObject) throws ClassNotFoundException {
 		theClass = newObject.getClass();
-//		theClass = Class.forName(newObject.getClass().getCanonicalName());
 		instance = newObject;
+	}
+
+	/**
+	 * Invokes a Method from an instance. This method gets
+	 * the Method's name from the ASTFunctionInvocation node,
+	 * parses the arguments from its ASTArgumentList node,
+	 * and returns the resulting Object.
+	 * 
+	 * @param {SimpleNode (ASTFunctionInvocation)} node
+	 * @param {Parser} p -- the active Parser's instance
+	 * @returns {Object} the Method's result
+	 */
+	public Object invoke(SimpleNode node, Parser p) {
+		// ASTDereference
+		final SimpleNode derefNode = (SimpleNode) node.jjtGetChild(0);
+
+		// Get the Method's name.
+		final String methodName = Parser.getTokenOfChild(derefNode, 0);
+
+		// ASTArgumentList
+		final SimpleNode argsNode = (SimpleNode) node.jjtGetChild(1);
+
+		try {
+			var args = parseArgs(argsNode, p);
+			final Method method =
+				theClass.getMethod(methodName, (Class<?>[]) args.get("paramTypes"));
+			method.setAccessible(true);
+			final Object result = method.invoke(instance, args.get("args"));
+			return getCorrespondingValue(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	/**
+	 * Parses Method arguments and returns them in the correct form.
+	 * 
+	 * @param {SimpleNode (ASTArgumentList)} argsNode -- the Method's arguments
+	 * @param {Parser} p -- active Parser's instance
+	 * @returns {HashMap<String, Object[]>} map containing the parameter types
+	 *          and the arguments in their 'raw type'
+	 * @throws ClassNotFoundException
+	 */
+	private HashMap<String, Object[]> parseArgs(SimpleNode argsNode, Parser p) throws ClassNotFoundException {
+		final HashMap<String, Object[]> result = new HashMap<String, Object[]>();
+
+		// Collect classes of the arguments to later find a matching Method.
+		Class<?>[] paramTypes = new Class<?>[0];
+		Object[] args = new Object[0];
+
+		final int numArgs = argsNode.jjtGetNumChildren();
+		if (numArgs > 0) {
+			// Re-initialise the arrays with the right length.
+			paramTypes = new Class<?>[numArgs];
+			args = new Object[numArgs];
+
+			for (int i = 0; i < numArgs; i++) {
+				final Object arg = p.doChild(argsNode, i).getRawValue();
+				paramTypes[i] = parsePrimitives(arg);
+				args[i] = arg;
+			}
+		}
+
+		result.put("paramTypes", paramTypes);
+		result.put("args", args);
+
+		return result;
+	}
+
+	/**
+	 * Adjusts class to match Method.getMethod() arguments.
+	 * 
+	 * @read https://stackoverflow.com/a/13943623/10620237
+	 * @param {Object} arg -- source argument whose class is to be returned
+	 * @returns {Class<?>} adjusted class that will match Method.getMethod()
+	 * @throws ClassNotFoundException
+	 */
+	private Class<?> parsePrimitives(Object arg) throws ClassNotFoundException {
+		final Class<?> perhapsPrimitive = Class.forName(arg.getClass().getName());
+
+		if (Boolean.class == perhapsPrimitive) return boolean.class;
+	    if (Byte.class == perhapsPrimitive)    return byte.class;
+	    if (Short.class == perhapsPrimitive)   return short.class;
+	    if (Integer.class == perhapsPrimitive) return int.class;
+	    if (Long.class == perhapsPrimitive)    return long.class;
+	    if (Float.class == perhapsPrimitive)   return float.class;
+	    if (Double.class == perhapsPrimitive)  return double.class;
+
+	    return perhapsPrimitive;
+	}
+
+	/**
+	 * Creates correct Value-type from primitives, or gives
+	 * a ValueReflection object for everything else.
+	 * 
+	 * @param {Object} methodResult
+	 * @returns {Value} Value-type corresponding to the resulting primitive
+	 * @throws ClassNotFoundException 
+	 */
+	private Value getCorrespondingValue(Object methodResult) throws ClassNotFoundException {
+		if (methodResult instanceof Boolean) return new ValueBoolean((boolean) methodResult);
+	    if (methodResult instanceof Short)   return new ValueInteger((short) methodResult);
+	    if (methodResult instanceof Integer) return new ValueInteger((int) methodResult);
+	    if (methodResult instanceof Long)    return new ValueInteger((long) methodResult);
+	    if (methodResult instanceof Float)   return new ValueRational((float) methodResult);
+	    if (methodResult instanceof Double)  return new ValueRational((double) methodResult);
+	    if (methodResult instanceof String)  return new ValueString((String) methodResult);
+
+	    return new ValueReflection(methodResult);
 	}
 
 	/**
@@ -138,116 +247,7 @@ public class ValueReflection extends ValueAbstract {
 	 */
 	@Override
 	public Value dereference(SimpleNode node, Value v, int currChild, Parser p) {
-		// TODO:
-//		final ValueReflection valueReflection = (ValueReflection) v;
-//		final String keyName = (node.jjtGetChild(currChild) instanceof ASTIdentifier)
-//			? Parser.getTokenOfChild(node, currChild)
-//			: p.doChild(node, currChild).toString();
-
 		return this;
-//		return null;
-//		return valueReflection.get(keyName);
-	}
-
-	// Invoke a Method from an instance.
-	/**
-	 * Invokes a Method from an instance. This method gets
-	 * the Method's name from the ASTFunctionInvocation node,
-	 * parses the arguments from its ASTArgumentList node,
-	 * and returns the resulting Object.
-	 * 
-	 * @param {SimpleNode (ASTFunctionInvocation)} node
-	 * @param {Parser} p -- the active Parser's instance
-	 * @returns {Object} the Method's result
-	 */
-	public Object invoke(SimpleNode node, Parser p) {
-		// ASTDereference
-		final SimpleNode derefNode = (SimpleNode) node.jjtGetChild(0);
-
-		// Get the Method's name.
-		final String methodName = Parser.getTokenOfChild(derefNode, 0);
-
-		// ASTArgumentList
-		final SimpleNode argsNode = (SimpleNode) node.jjtGetChild(1);
-
-		try {
-			var args = parseArgs(argsNode, p);
-			final Method method =
-				theClass.getMethod(methodName, (Class<?>[]) args.get("paramTypes"));
-			method.setAccessible(true);
-			return new ValueReflection(method.invoke(instance, args.get("args")));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return null;
-	}
-
-	/**
-	 * Parses Method arguments and returns them in the correct form.
-	 * 
-	 * @param {SimpleNode (ASTArgumentList)} argsNode -- the Method's arguments
-	 * @param {Parser} p -- active Parser's instance
-	 * @returns {HashMap<String, Object[]>} map containing the parameter types
-	 *          and the arguments in their 'raw type'
-	 * @throws ClassNotFoundException
-	 */
-	private HashMap<String, Object[]> parseArgs(SimpleNode argsNode, Parser p) throws ClassNotFoundException {
-		final HashMap<String, Object[]> result = new HashMap<String, Object[]>();
-
-		// Collect classes of the arguments to later find a matching Method.
-		Class<?>[] paramTypes = new Class<?>[0];
-		Object[] args = new Object[0];
-
-		final int numArgs = argsNode.jjtGetNumChildren();
-		if (numArgs > 0) {
-			// Re-initialise the arrays with the right length.
-			paramTypes = new Class<?>[numArgs];
-			args = new Object[numArgs];
-
-			for (int i = 0; i < numArgs; i++) {
-				final Object arg = p.doChild(argsNode, i).getRawValue();
-				paramTypes[i] = arg.getClass();
-				args[i] = arg;
-			}
-		}
-
-		result.put("paramTypes", paramTypes);
-		result.put("args", args);
-
-		return result;
-	}
-
-	/**
-	 * Execute a prototype function.
-	 * 
-	 * @param {String} protoFunc -- prototype function name
-	 * @param {ArrayList<Value>} protoArgs -- arguments for the function
-	 * @returns {Value} result of the prototype function
-	 */
-	@Override
-	public Value execProto(String protoFunc, ArrayList<Value> protoArgs) {
-		//TODO:
-		switch (protoFunc) {
-//			case "getClass":
-//				return new ValueString(getName());
-//			case "keys":
-//				return keys();
-//			case "remove":
-//				protoArgs.forEach(arg -> remove(arg.stringValue()));
-//				break;
-//			case "size":
-//			case "length":
-//				return new ValueInteger(size());
-//			case "tryRemove":
-//				protoArgs.forEach(arg -> tryRemove(arg.stringValue()));
-//				break;
-			default:
-				throw new ExceptionSemantic("There is no prototype function \""
-					+ protoFunc + "\" in " + getName() + " class.");
-		}
-
-//		return null;
 	}
 
 	/**
