11b528639dbbd55d0950d915dc762c925a13a741
amrwc <42574496+amrwc@users.noreply.github.com>
2019-06-03 23:13:12 +0200

    Improve error messages in ValueAbstract

--- Clove/src/interpreter/ExceptionSemantic.java
+++ Clove/src/interpreter/ExceptionSemantic.java
@@ -1,5 +1,7 @@
 package interpreter;
 
+import values.Value;
+
 /**
  * This exception is thrown when semantic errors are encountered.
  * 
@@ -10,5 +12,32 @@ public class ExceptionSemantic extends Error {
 
 	public ExceptionSemantic(String message) {
 		super(message);
+	}
+
+	/**
+	 * Throws an ExceptionSemantic regarding a binary operation.
+	 * 
+	 * @param {Value}  v1
+	 * @param {String} operation
+	 * @param {Value}  v2
+	 * @author amrwc
+	 */
+	public static ExceptionSemantic binaryOperationError(Value v1, String operation,
+			Value v2) {
+		return new ExceptionSemantic("Cannot perform '" + operation + "' on '"
+				+ v1.getName() + " (" + v1.getRawValue() + ")' and '" + v2.getName()
+				+ " (" + v2.getRawValue() + ")'.");
+	}
+
+	/**
+	 * Throws an ExceptionSemantic regarding an unary operation.
+	 * 
+	 * @param {Value}  v
+	 * @param {String} operation
+	 * @author amrwc
+	 */
+	public static ExceptionSemantic unaryOperationError(Value v, String operation) {
+		return new ExceptionSemantic("Cannot perform '" + operation + "' on '"
+				+ v.getName() + " (" + v.getRawValue() + ")'.");
 	}
 }
