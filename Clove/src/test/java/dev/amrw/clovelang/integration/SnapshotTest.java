74f564e97668357f5317cc83fb7bc4cfa5bec9de
amrwc <42574496+amrwc@users.noreply.github.com>
2021-11-27 18:50:32 +0100

    Split tests into fast and slow

--- Clove/src/test/java/dev/amrw/clovelang/integration/SnapshotTest.java
+++ Clove/src/test/java/dev/amrw/clovelang/integration/SnapshotTest.java
@@ -6,23 +6,23 @@ import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import dev.amrw.clovelang.interpreter.Interpreter;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.Comparator;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @Tag("integration-test")
-public class SnapshotTest {
+@DisplayName("Snapshot tests")
+class SnapshotTest {
 
-  private static final Pattern FILES_PREFIX = Pattern.compile(".*/resources/test/");
   private static PrintStream stdout;
 
   @BeforeAll
@@ -36,30 +36,41 @@ public class SnapshotTest {
     System.setOut(stdout);
   }
 
-  @Test
-  @DisplayName("The outputs of the first 10 Clove programs should match their snapshots")
-  void testRunsShouldMatchSnapshots_0to10() throws IOException {
-    executeBatch(0, 10);
+  @DisplayName("Fast tests")
+  @ParameterizedTest(name = "[{index}] filename: {0}")
+  @CsvSource({
+      "test01.clove",
+      "test02.clove",
+      "test03.clove",
+      "test04.clove",
+      "test05.clove",
+      "test06.clove",
+      "test07.clove",
+      "test08.clove",
+      "test09.clove",
+      "test10.clove"
+  })
+  void fastTests(final String filename) throws IOException {
+    snapshotTest("integration/" + filename);
   }
 
-  private void executeBatch(final int startIndex, final int endIndex) throws IOException {
-    final var integrationDirPath = getResourcePath("integration");
-    final var integrationFiles = new File(integrationDirPath).listFiles();
-    final var sortedIntegrationFiles = Stream.of(integrationFiles)
-        .sorted(Comparator.comparing(File::getAbsolutePath))
-        .toList();
+  @Nested
+  @Timeout(60)
+  @Tag("integration-test")
+  @DisplayName("Slow tests")
+  class SlowTests {
 
-    for (int i = startIndex * 2; i < endIndex * 2; i += 2) {
-      final var cloveFilePath = stripLeadingPath(sortedIntegrationFiles.get(i));
-      final var result = executeClove(cloveFilePath);
-      final var snapshotFilePath = stripLeadingPath(sortedIntegrationFiles.get(i + 1));
-      final var expectedOutput = getResourceAsString(snapshotFilePath);
-      assertThat(result).isEqualTo(expectedOutput);
+    @Test
+    @DisplayName("Fibonacci")
+    void fibonacci() throws IOException {
+      snapshotTest("integration/fibonacci.clove");
     }
   }
 
-  private String stripLeadingPath(final File file) {
-    return FILES_PREFIX.matcher(file.getAbsolutePath()).replaceAll("");
+  private void snapshotTest(final String resourcePath) throws IOException {
+    final var result = executeClove(resourcePath);
+    final var expectedOutput = getResourceAsString(resourcePath + ".snapshot");
+    assertThat(result).isEqualTo(expectedOutput);
   }
 
   private String executeClove(final String filePath) throws IOException {
@@ -76,11 +87,7 @@ public class SnapshotTest {
     }
   }
 
-  private String getResourcePath(final String path) {
-    return Resources.getResource(path).getPath();
-  }
-
-  private InputStream gerResourceAsStream(String path) throws IOException {
+  private static InputStream gerResourceAsStream(final String path) throws IOException {
     return Resources.getResource(path).openStream();
   }
 
