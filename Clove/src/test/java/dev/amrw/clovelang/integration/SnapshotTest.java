670392f2dcd16d7736dd2f8b684f84ba916ac0a1
amrwc <42574496+amrwc@users.noreply.github.com>
2021-11-28 01:59:57 +0100

    Revert "WIP: Parallelise test executions"
    This reverts commit 06a78f3d880104ae92b79ba8ea03ee19cb56c575.

--- Clove/src/test/java/dev/amrw/clovelang/integration/SnapshotTest.java
+++ Clove/src/test/java/dev/amrw/clovelang/integration/SnapshotTest.java
@@ -9,62 +9,31 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
-import org.junit.jupiter.api.parallel.Execution;
-import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 @Tag("integration-test")
 @DisplayName("Snapshot tests")
-@Execution(ExecutionMode.CONCURRENT)
 class SnapshotTest {
 
-  //  private static PrintStream stdout;
-//  private PrintStream stdout;
-  private static final ThreadLocal<PrintStream> stdout = new ThreadLocal<>();
+  private static PrintStream stdout;
 
-//  @BeforeAll
-//  static void beforeAll() {
-//    stdout = System.out;
-//  }
-//
-//  @AfterAll
-//  static void afterAll() {
-//    // Restore standard output stream
-//    System.setOut(stdout);
-//  }
-
-//  @BeforeEach
-//  void beforeEach() {
-//    stdout = System.out;
-//  }
-//
-//  @AfterEach
-//  void afterEach() {
-//    // Restore standard output stream
-//    System.setOut(stdout);
-//  }
-
-  @BeforeEach
-  void beforeEach() {
-    if (stdout.get() != null) {
-      System.out.println("stdout ThreadLocal is not empty");
-    }
-    stdout.set(System.out);
+  @BeforeAll
+  static void beforeAll() {
+    stdout = System.out;
   }
 
-  @AfterEach
-  void afterEach() {
+  @AfterAll
+  static void afterAll() {
     // Restore standard output stream
-    System.setOut(stdout.get());
-    stdout.remove();
+    System.setOut(stdout);
   }
 
   @DisplayName("Fast tests")
@@ -113,8 +82,7 @@ class SnapshotTest {
       System.setIn(fileStream);
       System.setOut(printStream);
       Interpreter.main(new String[]{});
-//      System.setOut(stdout);
-      System.setOut(stdout.get());
+      System.setOut(stdout);
       return outputStream.toString();
     }
   }
